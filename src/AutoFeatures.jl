2a493e5454a32a6f8eff4c107db1a69d4864afd8
evalparse <zhuojia.dai@gmail.com>
2019-09-23 10:00:01 +1000

    Files generated by PkgTemplates

---
+++ src/AutoFeatures.jl
@@ -0,0 +1,5 @@
+module AutoFeatures
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using AutoFeatures
+using Test
+
+@testset "AutoFeatures.jl" begin
+    # Write your own tests here.
+end
